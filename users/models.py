db38c79a6494fd6a4c4c32b0f802cb2849368cbe
wjdgodls7 <jhi4210@hanmail.net>
2022-04-08 17:04:38 +0900

    gogogo

---
+++ users/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+
+    """ Custom User Model """
+
+    GENDER_MAIL = "male"
+    GENDER_FEMAIL = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MAIL, "Male"),
+        (GENDER_FEMAIL, "Female"),
+        (GENDER_OTHER, "Other")
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUATGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUATGE_KOREAN, "Korean"),
+    )
+
+    CURRNECY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICE = (
+        (CURRNECY_USD, "USD"),
+        (CURRENCY_KRW, "KRW")
+    )
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    currency = models.CharField(choices=CURRENCY_CHOICE, max_length=3, blank=True)
+    superhost = models.BooleanField(default=False)
