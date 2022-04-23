0536394bcc599df2849e9c8fe3420de722f9ad84
wjdgodls7 <jhi4210@hanmail.net>
2022-04-23 21:01:20 +0900

    add sign up

--- users/views.py
+++ users/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.shortcuts import redirect, reverse
+from django.contrib.auth import authenticate, login, logout
+from . import forms
 
-# Create your views here.
+
+class LoginView(FormView):
+
+    template_name = "users/login.html"
+    form_class = forms.LoginForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
+
+
+def log_out(request):
+    logout(request)
+    return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+
+    template_name = "users/signup.html"
+    form_class = forms.SignupForm
+    success_url = reverse_lazy("core:home")
+    initial = {
+        "firstname": "Jung",
+        "lastname": "HAE IN",
+        "email": "jhi4210@hanmail.net"
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_valid(form)
