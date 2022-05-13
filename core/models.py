32f69127743c7beee90ec7a0bc548524cf2ee283
wjdgodls7 <jhi4210@hanmail.net>
2022-05-13 18:55:19 +0900

    end reservation

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from . import managers
 
 
 class TimeStampModel(models.Model):
@@ -7,6 +8,7 @@ class TimeStampModel(models.Model):
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    objects = managers.CustomModelManager()
 
     class Meta:
         abstract = True
--- reservations/models.py
+++ reservations/models.py
@@ -2,7 +2,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 from core import models as core_models
-from . import managers
 
 
 class BookedDay(core_models.TimeStampModel):
@@ -37,7 +36,6 @@ class Reservation(core_models.TimeStampModel):
     check_out = models.DateField()
     guest = models.ForeignKey("users.User", related_name="reservations", on_delete=models.CASCADE)
     room = models.ForeignKey("rooms.Room", related_name="reservations", on_delete=models.CASCADE)
-    objects = managers.CustomReservationManager()
 
     def __str__(self):
         return f'{self.room} - {self.check_in}'
@@ -49,7 +47,10 @@ class Reservation(core_models.TimeStampModel):
 
     def is_finished(self):
         now = timezone.now().date()
-        return now > self.check_out
+        is_finished = now > self.check_out
+        if is_finished:
+            BookedDay.objects.filter(reservation=self).delete()
+        return is_finished
     is_finished.boolean = True
 
     def save(self, *args, **kwargs):
