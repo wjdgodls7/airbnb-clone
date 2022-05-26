f3c6acf148114939a0535f72a6c535ab78c7ad7b
wjdgodls7 <jhi4210@hanmail.net>
2022-05-26 20:39:42 +0900

    commit

--- config/settings.py
+++ config/settings.py
@@ -177,8 +177,8 @@ LANGUAGE_COOKIE_NAME = "django_language"
 
 if not DEBUG:
 
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+    DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
+    STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
     AWS_STORAGE_BUCKET_NAME = "airbnb-clone-haeinee"
